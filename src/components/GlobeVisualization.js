8795277c8da75adb6e66ba9040c204bcbae5bfe4
thstasy <thstasy.hsieh@gmail.com>
2023-09-07 16:16:49 +0800

    add

--- src/components/GlobeVisualization.js
+++ src/components/GlobeVisualization.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Globe from 'globe.gl';
 import '../../src/App.css';
-import Alert from "./Alert.js"; 
+import Alert from "./Alert.js";
 import Footer from "../components/Footer.js";
 import httpClient from "../http-common.js";
-
-
 
 function GlobeVisualization() {
   const globeRef = useRef(null);
@@ -15,48 +13,55 @@ function GlobeVisualization() {
   const RINGS_MAX_R = 5;
   const RING_PROPAGATION_SPEED = 5;
   const [email, setEmail] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
+
   const [alertMessage, setAlertMessage] = useState("");
-  const [alertType, setAlertType] = useState("notification");
+  const [alertType, setAlertType] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleShowAlert = (message, type) => {
+    setAlertMessage(message);
+    setAlertType(type);
+    setShowAlert(true);
+  };
 
   const subscribe = async (event) => {
     event.preventDefault();
-  
+
     const headers = {
       'Content-Type': 'application/json',
     };
-  
+
     const data = {
       email: email,
     };
-  
+
     try {
       const response = await httpClient.post('/subscribe', data, {
         headers,
         withCredentials: true,
       });
-  
-      if (response.status === 200) {
-        // setAlertMessage("Please confirm the email in your mailbox in 10 minutes.");
-        // setAlertType("notification");
-        alert("Please confirm the email in your mailbox in 10 minutes.");
-      } else {
-        // setAlertMessage("Subscription failed.");
-        // setAlertType("notification");
-        alert("Subscription failed.");
+      if (response.status === 201) {
+        handleShowAlert("An email will be sent to your mailbox for confirmation.", "notification");
+        alert("An email will be sent to your mailbox for confirmation.");
+      } else if (response.status === 409) {
+        const responseData = await response.json();
+        if (responseData.message === "Invalid Email" || responseData.message === "Email Already Subscribed") {
+          handleShowAlert("Invalid Email: Please provide a valid email address.", "notification");
+          alert("Invalid Email: Please provide a valid email address.");
+        } else {
+          handleShowAlert("Subscription failed.", "notification");
+          alert("Subscription failed.");
+        }
+      } else if (response.status === 500) {
+        handleShowAlert("Apologies for internal server error, we'll fix it asap.", "notification");
+        alert("Internal Server error. ");
       }
     } catch (error) {
-      alert('An error occurred.');
-      // setAlertMessage('An error occurred.');
-      // setAlertType('notification');
+      handleShowAlert("Apologies for internal server error, we'll fix it asap.", "notification");
+      alert("An error occurred.");
       console.error('Error:', error);
     }
-  };
-  
-    
-  const handleHideAlert = () => {
-    setShowAlert(false);
-  };
+  }
 
   useEffect(() => {
     const globe = Globe()
@@ -72,7 +77,6 @@ function GlobeVisualization() {
       .ringMaxRadius(RINGS_MAX_R)
       .ringPropagationSpeed(RING_PROPAGATION_SPEED)
       .ringRepeatPeriod(FLIGHT_TIME * ARC_REL_LEN / NUM_RINGS)
-      //.globeDistanceRadiusScale(0)
       (globeRef.current);
 
     let prevCoords = { lat: 0, lng: 0 };
@@ -100,64 +104,64 @@ function GlobeVisualization() {
   }, []);
 
   return (
-<>
-    {showAlert && (
-      <Alert
-        onClose={handleHideAlert}
-        message={alertMessage}
-        type={alertType}
-      />
-    )}
-    <div className="container" >
-      <div className="row">
-        <div className="col-sm-8">
-          <div className="d-flex flex-column align-items-left text-container">
-            <h2 style={{ margin: '0em 0em 0.2em 0em', fontWeight: 'bolder', wordBreak: 'break-word' }}>
-            TO SUBSCRIBE, ENTER YOUR EMAIL
-            </h2>
-            <h5>
-            What You'll Get From the Subscription: A Story Of My Week 
-            </h5>
-            <h6 style={{fontWeight:"normal"}}>
-            interesting articles related to the topics I saw on Medium and books, ideas popped up from conversations, and most importantly---a real human perspective. 
-            </h6>
-            <div>
-              <div className="form-group">
-                <form onSubmit={subscribe}>
-                <label htmlFor="email">Email <small className="reminder-text">(Please provide a valid email address for confirmation)</small></label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="Enter Email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-                  <button
-                    type="submit"
-                    style={{
-                      backgroundColor: 'black',
-                      color: 'white',
-                      borderRadius: '5px',
-                      marginTop: '0.5em',
-                      marginBottom:"0.5em"
-                    }}
-                  >
-                    Submit
-                  </button>
+    <>
+      {showAlert && (
+        <Alert
+          message={alertMessage}
+          type={alertType}
+          onClose={() => setShowAlert(false)}
+        />
+      )}
+      <div className="container" >
+        <div className="row">
+          <div className="col-sm-8">
+            <div className="d-flex flex-column align-items-left text-container">
+              <h2 style={{ margin: '0em 0em 0.2em 0em', fontWeight: 'bolder', wordBreak: 'break-word' }}>
+                TO SUBSCRIBE, ENTER YOUR EMAIL
+              </h2>
+              <h5>
+                What You'll Get From the Subscription: A Story Of My Week
+              </h5>
+              <h6 style={{ fontWeight: "normal" }}>
+                articles related to the topics on Medium and books, ideas from conversations, with a real human being's perspective.
+              </h6>
+              <div>
+                <div className="form-group">
+                  <form onSubmit={subscribe}>
+                    <label htmlFor="email">Email <small className="reminder-text">(Please provide a valid email address for confirmation)</small></label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Enter Email"
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <button
+                      type="submit"
+                      style={{
+                        backgroundColor: 'black',
+                        color: 'white',
+                        borderRadius: '5px',
+                        marginTop: '0.5em',
+                        marginBottom: "0.5em"
+                      }}
+                    >
+                      Submit
+                    </button>
 
-                </form>
+                  </form>
+                </div>
               </div>
-            </div>
-            <div className="globe-container">
-              <p className="overlay-text">Click on the globe to see how signals transmit!</p>
-              <div ref={globeRef} style={{ cursor: 'pointer', width: '100%', height: '100%' }}></div>
+              <div className="globe-container">
+                <p className="overlay-text">Click on the globe to see how signals transmit!</p>
+                <div ref={globeRef} style={{ cursor: 'pointer', width: '100%', height: '100%' }}></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br />
       <Footer />
     </>
   );
@@ -165,27 +169,3 @@ function GlobeVisualization() {
 
 export default GlobeVisualization;
 
-
-
-//   fetch('/subscribe', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ email }),
-  //   })
-  //     .then((response) => {
-  //       if (response.status === 200) {
-  //         setAlertMessage("Please confirm the email in your mailbox in 10 minutes.");
-  //         setAlertType("notification");
-  //       } else {
-  //         setAlertMessage("Subscription failed.");
-  //         setAlertType("notification");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       setAlertMessage("Error!");
-  //       setAlertType("notification");
-  //       console.error('Error:', error);
-  //     });
-  // };
